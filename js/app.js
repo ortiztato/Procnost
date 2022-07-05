ed98cde533f2c3729e00dfd75330403105a25bab
unknown <ortiztato@gmail.com>
2022-07-04 18:41:14 -0600

    carrousel + arreglos varios

--- js/app.js
+++ js/app.js
@@ -13,3 +13,40 @@ openMenuBtn.addEventListener("click", toggleMenu);
 closeMenuBtn.addEventListener("click", toggleMenu);
 contactbtn.addEventListener("click", toggleMenu);
 productosbtn.addEventListener("click", toggleMenu);
+
+
+let indice = 1;
+muestraSlides(indice);
+
+function avanzaSlide(n) {
+  muestraSlides(indice += n);
+}
+
+function posicionSlide(n) {
+  muestraSlides(indice = n);
+}
+setInterval(function tiempo() {
+  muestraSlides(indice += 1)
+}, 6000);
+function muestraSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName('miSlider');
+  let barras = document.getElementsByClassName('barra');
+
+  if (n > slides.length) {
+    indice = 1;
+  }
+  if (n < 1) {
+    indice = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = 'none';
+  }
+  for (i = 0; i < barras.length; i++) {
+    barras[i].className = barras[i].className.replace(" active", "");
+  }
+
+  slides[indice - 1].style.display = 'block';
+  barras[indice - 1].className += ' active';
+
+}
