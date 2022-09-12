c2e15321fed94f9a33973afcb6704063d72eb868
unknown <ortiztato@gmail.com>
2022-09-12 10:43:52 -0300

    mod event list a productos button

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@ const menu = document.querySelector(".menu");
 const openMenuBtn = document.querySelector(".open-menu");
 const closeMenuBtn = document.querySelector(".close-menu");
 const contactbtn = document.querySelector("#contactbtn");
-const productosbtn = document.querySelector("#productosbtn");
+/* const productosbtn = document.querySelector("#productosbtn"); */
 
 
 function toggleMenu() {
@@ -12,7 +12,7 @@ function toggleMenu() {
 openMenuBtn.addEventListener("click", toggleMenu);
 closeMenuBtn.addEventListener("click", toggleMenu);
 contactbtn.addEventListener("click", toggleMenu);
-productosbtn.addEventListener("click", toggleMenu);
+/* productosbtn.addEventListener("click", toggleMenu); */
 
 /* 
 let indice = 1;
